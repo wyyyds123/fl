19011f0a046469ce14b11e3933fc3ae96fc55240
wyyyds123 <112397311+wyyyds123@users.noreply.github.com>
2023-05-18 08:17:17 +0800

    更新 main.py

--- main.py
+++ main.py
@@ -1,77 +1,78 @@
-import random
-from time import localtime
-from requests import get, post
-from datetime import datetime, date
-from zhdate import ZhDate
-import sys
-import os
-
-
-def get_color():
-    # 获取随机颜色
-    get_colors = lambda n: list(map(lambda i: "#" + "%06x" % random.randint(0, 0xFFFFFF), range(n)))
-    color_list = get_colors(100)
-    return random.choice(color_list)
-
-
-def get_access_token():
-    # appId
-    app_id = config["app_id"]
-    # appSecret
-    app_secret = config["app_secret"]
-    post_url = ("https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid={}&secret={}"
-                .format(app_id, app_secret))
-    try:
-        access_token = get(post_url).json()['access_token']
-    except KeyError:
-        print("获取access_token失败，请检查app_id和app_secret是否正确")
-        os.system("pause")
-        sys.exit(1)
-    # print(access_token)
-    return access_token
-
-
-def get_weather(region):
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
-                      'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'
-    }
-    key = config["weather_key"]
-    region_url = "https://geoapi.qweather.com/v2/city/lookup?location={}&key={}".format(region, key)
-    response = get(region_url, headers=headers).json()
-    if response["code"] == "404":
-        print("推送消息失败，请检查地区名是否有误！")
-        os.system("pause")
-        sys.exit(1)
-    elif response["code"] == "401":
-        print("推送消息失败，请检查和风天气key是否正确！")
-        os.system("pause")
-        sys.exit(1)
-    else:
-        # 获取地区的location--id
-        location_id = response["location"][0]["id"]
-    weather_url = "https://devapi.qweather.com/v7/weather/now?location={}&key={}".format(location_id, key)
-    response = get(weather_url, headers=headers).json()
-    # 天气
-    weather = response["now"]["text"]
-    # 当前温度
-    temp = response["now"]["temp"] + u"\N{DEGREE SIGN}" + "C"
-    # 风向
-    wind_dir = response["now"]["windDir"]
-    # 获取逐日天气预报
-    url = "https://devapi.qweather.com/v7/weather/3d?location={}&key={}".format(location_id, key)
-    response = get(url, headers=headers).json()
-    # 最高气温
-    max_temp = response["daily"][0]["tempMax"] + u"\N{DEGREE SIGN}" + "C"
-    # 最低气温
-    min_temp = response["daily"][0]["tempMin"] + u"\N{DEGREE SIGN}" + "C"
-    # 日出时间
-    sunrise = response["daily"][0]["sunrise"]
-    # 日落时间
-    sunset = response["daily"][0]["sunset"]
-    url = "https://devapi.qweather.com/v7/air/now?location={}&key={}".format(location_id, key)
-    response = get(url, headers=headers).json()
-    if response["code"] == "200":
+# 修改推送消息为简体中文
+import random 
+from time import localtime 
+from requests import get, post 
+from datetime import datetime, date 
+from zhdate import ZhDate 
+import sys 
+import os 
+  
+  
+def get_color(): 
+    # 获取随机颜色 
+    get_colors = lambda n: list(map(lambda i: "#" + "%06x" % random.randint(0, 0xFFFFFF), range(n))) 
+    color_list = get_colors(100) 
+    return random.choice(color_list) 
+  
+  
+def get_access_token(): 
+    # appId 
+    app_id = config["app_id"] 
+    # appSecret 
+    app_secret = config["app_secret"] 
+    post_url = ("https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid={}&secret={}" 
+                .format(app_id, app_secret)) 
+    try: 
+        access_token = get(post_url).json()['access_token'] 
+    except KeyError: 
+        print("获取access_token失败，请检查app_id和app_secret是否正确") 
+        os.system("pause") 
+        sys.exit(1) 
+    # print(access_token) 
+    return access_token 
+  
+  
+def get_weather(region): 
+    headers = { 
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) ' 
+                      'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36' 
+    } 
+    key = config["weather_key"] 
+    region_url = "https://geoapi.qweather.com/v2/city/lookup?location={}&key={}".format(region, key) 
+    response = get(region_url, headers=headers).json() 
+    if response["code"] == "404": 
+        print("推送消息失败，请检查地区名是否有误！") 
+        os.system("pause") 
+        sys.exit(1) 
+    elif response["code"] == "401": 
+        print("推送消息失败，请检查和风天气key是否正确！") 
+        os.system("pause") 
+        sys.exit(1) 
+    else: 
+        # 获取地区的location--id 
+        location_id = response["location"][0]["id"] 
+    weather_url = "https://devapi.qweather.com/v7/weather/now?location={}&key={}".format(location_id, key) 
+    response = get(weather_url, headers=headers).json() 
+    # 天气 
+    weather = response["now"]["text"] 
+    # 当前温度 
+    temp = response["now"]["temp"] + u"\N{DEGREE SIGN}" + "C" 
+    # 风向 
+    wind_dir = response["now"]["windDir"] 
+    # 获取逐日天气预报 
+    url = "https://devapi.qweather.com/v7/weather/3d?location={}&key={}".format(location_id, key) 
+    response = get(url, headers=headers).json() 
+    # 最高气温 
+    max_temp = response["daily"][0]["tempMax"] + u"\N{DEGREE SIGN}" + "C" 
+    # 最低气温 
+    min_temp = response["daily"][0]["tempMin"] + u"\N{DEGREE SIGN}" + "C" 
+    # 日出时间 
+    sunrise = response["daily"][0]["sunrise"] 
+    # 日落时间 
+    sunset = response["daily"][0]["sunset"] 
+    url = "https://devapi.qweather.com/v7/air/now?location={}&key={}".format(location_id, key) 
+    response = get(url, headers=headers).json() 
+    if response["code"] == "200": 
         # 空气质量
         category = response["now"]["category"]
         # pm2.5
